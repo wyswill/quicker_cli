c69464ccbc8bdf4496d735c72235f047e60ca77a
wyswill <1151373571@qq.com>
2019-09-26 17:27:04 +0800

    添加多选项目功能

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const program = require("commander");
-const download = require("download-git-repo");
+const download = require("download-git-repo");//从git仓库下载
 const handlebars = require("handlebars");
 const inquirer = require("inquirer");
-const ora = require("ora");
+const ora = require("ora");//控制台的输出颜色
 const chalk = require("chalk");
 const symbols = require("log-symbols");
 program
-  .version("1.0.0", "-v, --version")
+  .version(require("./package").version, "-v, --version")
   .command("init <name>")
   .action(name => {
     if (!fs.existsSync(name)) {
       inquirer
         .prompt([
           {
-            name: "description",
-            message: "请输入项目描述"
+            message: "选择要创建的项目框架：",
+            name: "project",
+            type: "list",
+            choices: ["vue", "react"]
+          },
+          {
+            message: "选择项目类型:",
+            name: "type",
+            type: "list",
+            choices: ["前台项目", "后台项目"]
           },
           {
             name: "author",
@@ -24,29 +32,42 @@ program
           }
         ])
         .then(answers => {
-          const spinner = ora("正在下载模板...");
+          const spinner = ora("初始化项目中...");
           spinner.start();
-          let template_url = `direct:https://github.com/wyswill/vue-project-templeates.git#master`;
-          download(template_url, name, { clone: true }, err => {
-            if (err) {
-              spinner.fail();
-              console.log(symbols.error, chalk.red(err));
-            } else {
-              spinner.succeed();
-              const fileName = `${name}/package.json`;
-              const meta = {
-                name,
-                description: answers.description,
-                author: answers.author
-              };
-              if (fs.existsSync(fileName)) {
-                const content = fs.readFileSync(fileName).toString();
-                const result = handlebars.compile(content)(meta);
-                fs.writeFileSync(fileName, result);
-              }
-              console.log(symbols.success, chalk.green("项目初始化完成"));
-            }
-          });
+          const {project, type, author} = answers;
+          console.log({project, type, author});
+          let breath = "";
+          switch (type) {
+            case "前台项目":
+              breath = `${project}_project`;
+              return;
+            case "后台项目":
+              breath = `${project}_admin`;
+              return;
+          }
+
+          let template_url = `direct:https://github.com/wyswill/vue-project-templeates.git#${breath}`;
+          // download(template_url, name, {clone: true}, err => {
+          //   if (err) {
+          //     spinner.fail();
+          //     console.log(symbols.error, chalk.red(err));
+          //   } else {
+          //     spinner.succeed();
+          //     const fileName = `${name}/package.json`;
+          //     const meta = {
+          //       name,
+          //       description: answers.description,
+          //       author: answers.author
+          //     };
+          //     if (fs.existsSync(fileName)) {
+          //       const content = fs.readFileSync(fileName).toString();
+          //       const result = handlebars.compile(content)(meta);
+          //       fs.writeFileSync(fileName, result);
+          //     }
+          //     console.log(symbols.success, chalk.green("项目初始化完成"));
+          //     console.log(symbols.success, chalk.green(`cd ${fileName} && npm run install`));
+          //   }
+          // });
         });
     } else {
       console.log(symbols.error, chalk.red("项目已存在"));
