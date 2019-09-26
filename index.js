1fc813bba7ca4a04596234d63cd2080f78546dc9
wyswill <1151373571@qq.com>
2019-09-26 18:04:39 +0800

    实现项目多选以及下载功能

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ program
             message: "选择项目类型:",
             name: "type",
             type: "list",
-            choices: ["前台项目", "后台项目"]
+            choices: ["project", "admin"]
           },
           {
             name: "author",
@@ -35,39 +35,29 @@ program
           const spinner = ora("初始化项目中...");
           spinner.start();
           const {project, type, author} = answers;
-          console.log({project, type, author});
-          let breath = "";
-          switch (type) {
-            case "前台项目":
-              breath = `${project}_project`;
-              return;
-            case "后台项目":
-              breath = `${project}_admin`;
-              return;
-          }
-
+          let breath = `${project}_${type}`;
           let template_url = `direct:https://github.com/wyswill/vue-project-templeates.git#${breath}`;
-          // download(template_url, name, {clone: true}, err => {
-          //   if (err) {
-          //     spinner.fail();
-          //     console.log(symbols.error, chalk.red(err));
-          //   } else {
-          //     spinner.succeed();
-          //     const fileName = `${name}/package.json`;
-          //     const meta = {
-          //       name,
-          //       description: answers.description,
-          //       author: answers.author
-          //     };
-          //     if (fs.existsSync(fileName)) {
-          //       const content = fs.readFileSync(fileName).toString();
-          //       const result = handlebars.compile(content)(meta);
-          //       fs.writeFileSync(fileName, result);
-          //     }
-          //     console.log(symbols.success, chalk.green("项目初始化完成"));
-          //     console.log(symbols.success, chalk.green(`cd ${fileName} && npm run install`));
-          //   }
-          // });
+          console.log(template_url);
+          download(template_url, name, {clone: true}, err => {
+            if (err) {
+              spinner.fail();
+              console.log(symbols.error, chalk.red(err));
+            } else {
+              spinner.succeed();
+              const fileName = `${name}/package.json`;
+              const meta = {
+                name,
+                author: answers.author
+              };
+              if (fs.existsSync(fileName)) {
+                const content = fs.readFileSync(fileName).toString();
+                const result = handlebars.compile(content)(meta);
+                fs.writeFileSync(fileName, result);
+              }
+              console.log(symbols.success, chalk.green("项目初始化完成"));
+              console.log(symbols.success, chalk.green(`cd ${fileName} && npm run install`));
+            }
+          });
         });
     } else {
       console.log(symbols.error, chalk.red("项目已存在"));
